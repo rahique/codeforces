820783fb18a29776dcdcdbdc69732e5df5e07af0
Rahique <rahiqueosman@gmail.com>
2024-05-24 23:11:49 +0600

    solved next round

--- c/nextRound.c
+++ c/nextRound.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-  int n, k;
+  int n, k, t=0;
+  int *array;
   scanf("%d%d", &n, &k);
-  
+  //allocating memory
+  array = (int*) malloc(n *sizeof(int));
+  //taking points input
+  for(int i=0;i<n;i++){
+    scanf("%d", &array[i]);
+  }
+  //checking next round participants
+  for(int i=0;i<n;i++){
+    if(array[i]>=k){
+      t++;
+    }
+  }
+  printf("%d\n", t);
+  free(array);
   return 0;
 }
